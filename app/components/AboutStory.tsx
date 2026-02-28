03d956481ece66f3fc0102534b861b6046f2f11f
captainaraf <shaidozzamanaraf21@gmail.com>
2026-03-01 04:03:50 +0600

    Fixed build errors

--- app/components/AboutStory.tsx
+++ app/components/AboutStory.tsx
@@ -20,7 +20,7 @@ const AboutStory = () => {
         visible: {
             opacity: 1,
             y: 0,
-            transition: { duration: 0.8, ease: [0.16, 1, 0.3, 1] },
+            transition: { duration: 0.8 },
         },
     };
 
